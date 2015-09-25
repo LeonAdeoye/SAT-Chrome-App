ca0563d636409fe1a6bc8d316737d716c08c974d
LeonAdeoye <leon.adeoye@gmail.com>
2015-09-26 00:23:52 +0800

    added file request error handler

--- javascript/main1.js
+++ javascript/main1.js
@@ -1,8 +1,33 @@
 window.onload = function() 
 {
+  var requestFileSystem = window.webkitRequestFileSystem || window.requestFileSystem;
+  requestFileSystem(PERSISTENT, 0, haveFileSystem, errorHandler);
+  
   $('#name-id').html('Hello, Leon!');
-  $('#colour-me').click(function()
+  $('#save-me').click(function()
   {
-    $('#colour-me').toggleClass("click-colour");
+    $('#save-me').toggleClass("click-colour");
   });
 };
+
+function errorHandler(e)
+{
+  var msg;
+  if(e.target && e.target.error)
+    e = e.target.error;
+  if(e.message)
+    msg = e.message;
+  else if (e.name)
+    msg = e.name;
+  else if (e.code)
+    msg = "Code: " + e.code;
+  else
+    msg = e.toString();
+    
+  showMessage("Error: " + msg);
+}
+
+function showMessage(msg)
+{
+  console.log(msg);
+}
