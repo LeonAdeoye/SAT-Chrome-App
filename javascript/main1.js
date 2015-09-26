c814b309343ac006d8c4ece4544ab0a0a7d75096
LeonAdeoye <leon.adeoye@gmail.com>
2015-09-26 23:36:11 +0800

    added socket creation

--- javascript/main1.js
+++ javascript/main1.js
@@ -7,6 +7,28 @@ window.onload = function()
   $('#save-me').click(function()
   {
     $('#save-me').toggleClass("click-colour");
+    
+    var req = "GET /data/2.1/find/name?units=imperial&q=Chicago HTTP/1.1\r\n" +
+    "Host: api.openweathermap.org\r\n\r\n";
+    
+    
+    chrome.sockets.tcp.create({}, function(createInfo)
+    {
+      console.log(createInfo.socketId);
+      chrome.sockets.tcp.connect(createInfo.socketId, "api.openweathermap.org", 80, function(result)
+      {
+        if(chrome.runtime.lastError)
+          console.log(chrome.runtime.lastError.message);
+        else
+          console.log(result);
+          chrome.sockets.tcp.send(createInfo.socketId, str2ab(req), function(sendInfo) {
+            console.log(sendInfo);
+          });
+      });
+    });
+    
+    
+    
   });
 };
 
